e37732a2b3dacec1a223b0c95dbda8db4c97d5b3
Deepak <dchand5986@gmail.com>
2023-07-17 20:44:58 +1000

    Modified help screen.

--- ADHDTaskManagerApp/app/src/main/java/com/csu_itc303_team1/adhdtaskmanager/HelpScreen.kt
+++ ADHDTaskManagerApp/app/src/main/java/com/csu_itc303_team1/adhdtaskmanager/HelpScreen.kt
@@ -1,55 +1,37 @@
 package com.csu_itc303_team1.adhdtaskmanager
 
+
 import android.annotation.SuppressLint
-import android.text.style.LineHeightSpan
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.*
-
-
-
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material3.*
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextDecoration
-
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 
@@ -88,19 +70,7 @@ fun HelpScreen() {
             Spacer(modifier = Modifier.height(15.dp))
 
 
-//            Text(text = "- View Manual",
-//                style = TextStyle(
-//                    fontSize = 20.sp),
-//            modifier = Modifier.clickable {
-//                // Open the user manual in a WebView or navigate to another screen.
-//                coroutineScope.launch {
-//
-//                    // For simplicity,launching an Intent to open a web URL.
-//
-//                    val intent = Intent(context, WebViewActivity::class.java)
-//                    context.startActivity(intent)
-//                }
-//            })
+
             // "View Manual" row
             Row(
                 modifier = Modifier.clickable {
