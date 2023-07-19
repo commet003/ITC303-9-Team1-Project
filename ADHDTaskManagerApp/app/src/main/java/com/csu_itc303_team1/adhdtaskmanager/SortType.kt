3dc8c04b6fc9aa96858097fa82f8a5a9cd792269
Corie Rhodes <commet003@users.noreply.github.com>
2023-07-19 15:40:52 +1000

    Added the ability for users to filter their task

--- ADHDTaskManagerApp/app/src/main/java/com/csu_itc303_team1/adhdtaskmanager/SortType.kt
+++ ADHDTaskManagerApp/app/src/main/java/com/csu_itc303_team1/adhdtaskmanager/SortType.kt
@@ -3,5 +3,7 @@ package com.csu_itc303_team1.adhdtaskmanager
 enum class SortType {
     BY_DATE,
     BY_PRIORITY,
-    BY_TIME
+    BY_TIME,
+    BY_COMPLETED,
+    BY_NOT_COMPLETED
 }
--- ADHDTaskManagerApp/app/src/main/java/com/csu_itc303_team1/adhdtaskmanager/TodoDao.kt
+++ ADHDTaskManagerApp/app/src/main/java/com/csu_itc303_team1/adhdtaskmanager/TodoDao.kt
@@ -22,12 +22,20 @@ interface TodoDao {
     fun sortByPriority(): Flow<List<Todo>>
 
     // Sort Todos by Due Date
-    @Query("SELECT * FROM Todo ORDER BY dueDate DESC")
+    @Query("SELECT * FROM Todo ORDER BY dueDate ASC")
     fun sortByDueDate(): Flow<List<Todo>>
 
     // Sort Todos by Due Time
-    @Query("SELECT * FROM Todo ORDER BY dueTime DESC")
+    @Query("SELECT * FROM Todo ORDER BY dueTime ASC")
     fun sortByDueTime(): Flow<List<Todo>>
+
+    // Sort Todos by isCompleted
+    @Query("SELECT * FROM Todo WHERE isCompleted = 1")
+    fun sortByCompleted(): Flow<List<Todo>>
+
+    // Sort Todos by not isCompleted
+    @Query("SELECT * FROM Todo WHERE isCompleted = 0")
+    fun sortByNotCompleted(): Flow<List<Todo>>
 
     @Update
     suspend fun updateTodoIsCompleted(todo: Todo)
